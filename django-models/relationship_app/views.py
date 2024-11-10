f7eb24cbf1d4eed4c5fd098d538d433070d7bda6
Brian Ngugi <ngugibrian00@gmail.com>
2024-11-10 17:54:44 +0300

    Added function-based view for book list and class-based view for library details

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
-
+from django.views.generic import DetailView
+from .models import Library
 # Create your views here.
+from django.shortcuts import render
+from .models import Book
+
+# Function-based view to list all books and their authors
+def book_list(request):
+    # Retrieve all books from the database
+    books = Book.objects.all()
+
+    # Pass the books to the template for rendering
+    return render(request, 'relationship_app/book_list.html', {'books': books})
+
+
+# Class-based view to display details of a specific library and its books
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'  # Specify the template
+    context_object_name = 'library'  # Name to reference the object in the template
