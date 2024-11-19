9c0cb74fe52930623ca6c130f80e4b012cc82fb8
Brian Ngugi <ngugibrian00@gmail.com>
2024-11-19 17:44:24 +0300

    Add custom user model, user manager, and admin integration

--- advanced_features_and_security/LibraryProject/relationship_app/models.py
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -8,7 +8,7 @@ from django.conf import settings
 from django.contrib.auth.models import BaseUserManager
 from django.utils.translation import gettext_lazy as _
 from .managers import CustomUserManager 
-
+from django.contrib.auth.models import BaseUserManager
 
 # Author model
 class Author(models.Model):
@@ -89,4 +89,23 @@ class CustomUserManager(BaseUserManager):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
 
+        return self.create_user(username, email, password, **extra_fields)
+
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, email, password=None, **extra_fields):
+        """Create and return a regular user with an email and password."""
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password=None, **extra_fields):
+        """Create and return a superuser with an email and password."""
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
         return self.create_user(username, email, password, **extra_fields)
