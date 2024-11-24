9e220e035a2f8a83f29faf811998c1ccd242f240
Brian Ngugi <ngugibrian00@gmail.com>
2024-11-24 22:10:42 +0300

    Added TokenAuthentication and permissions to secure the API

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -3,19 +3,18 @@ from .views import BookList
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import BookList, BookViewSet
-urlpatterns = [
-    path('books/', BookList.as_view(), name='book-list'),  # Maps to the BookList view
-]
-
-
-# Create a router and register the BookViewSet
+from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
 router = DefaultRouter()
 router.register(r'books_all', BookViewSet, basename='book_all')
 
 urlpatterns = [
-    # Route for the BookList view (ListAPIView)
+    path('books/', BookList.as_view(), name='book-list'),  # Maps to the BookList view
+    path('auth/token/', obtain_auth_token, name='api_token_auth'),
+    path('', include(router.urls)),
     path('books/', BookList.as_view(), name='book-list'),
 
     # Include the router URLs for BookViewSet (all CRUD operations)
-    path('', include(router.urls)),  # Includes all routes registered with the router
+    path('', include(router.urls)), 
+
 ]
